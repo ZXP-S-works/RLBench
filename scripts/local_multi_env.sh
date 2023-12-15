ef78f38c9b2eb485c885bd4d90adf5df13fe5002
ZXP-S-Works <zxpzzz521@gmail.com>
2023-12-15 16:11:40 -0500

    improve robustness of gripper open/close info

--- scripts/local_multi_env.sh
+++ scripts/local_multi_env.sh
@@ -5,7 +5,7 @@ args="
 --save_path=/home/zxp/projects/C2F_bi_equi/c2f_bi_equi_data/${train_val}
 --image_size=128,128
 --renderer=opengl
---episodes_per_task=10
+--episodes_per_task=100
 --processes=1
 --all_variations=False
 --variations=0
@@ -16,17 +16,28 @@ jn=${run_name}_${gpu}
 ./local_env.sh ${args}
 }
 
+#task_names=(
+#'place_wine_at_rack_location'
+#'place_cups'
+#'light_bulb_in'
+#'put_groceries_in_cupboard'
+#'slide_block_to_color_target'
+#'sweep_to_dustpan_of_size'
+#'stack_blocks'
+#'close_jar'
+#'insert_onto_square_peg'
+#'put_money_in_safe'
+#'meat_off_grill'
+#'open_drawer'
+#'reach_and_drag'
+#'push_buttons'
+#'stack_cups'
+#'turn_tap'
+#'put_item_in_drawer'
+#'place_shape_in_shape_sorter'
+#)
+
 task_names=(
-'place_wine_at_rack_location'
-'place_cups'
-'light_bulb_in'
-'put_groceries_in_cupboard'
-'slide_block_to_color_target'
-'sweep_to_dustpan_of_size'
-'stack_blocks'
-'close_jar'
-'insert_onto_square_peg'
-'put_money_in_safe'
 'meat_off_grill'
 'open_drawer'
 'reach_and_drag'
@@ -39,7 +50,7 @@ task_names=(
 
 for tasks in "${task_names[@]}"
   do
-    for train_val in 'train' 'val'
+    for train_val in 'val'
     do
       run
       done
